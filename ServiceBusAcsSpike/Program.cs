a957ca14fe2d14e99f7e2e5dfca2233e901aa826
Luis Sotomayor <LSotomayor@futuredontics.com>
2014-06-04 17:06:37 -0700

    Initial commit

---
+++ ServiceBusAcsSpike/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+
+namespace ServiceBusAcsSpike
+{
+    internal class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof (Program));
+
+        private static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            try
+            {
+                var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString.Sender");
+                Log.DebugFormat("Connection string: {0}", connectionString);
+
+                var client = TopicClient.CreateFromConnectionString(connectionString, "foo/bar");
+                client.Send(new BrokeredMessage("This is a test"));
+
+                Log.DebugFormat("Message sent");
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
+        }
+    }
+}
